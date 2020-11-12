9c0bf581ad6a95f16865217e6f6b4bcb9e214a8a
flonitoo <73523574+flonitoo@users.noreply.github.com>
2020-11-12 12:13:53 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('flonitoo/docker-nodejs-demo')
+            repositoryName('flonhoxhaa/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
